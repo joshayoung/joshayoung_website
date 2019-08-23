12e428ddab88eaf80a08485811caa037a3d8d5e9
Josh Young <j22cal@gmail.com>
2019-08-23 13:05:19 -0500

    Add config to transform markdown into a page, add sample page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,15 +4,40 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-module.exports = {
-  plugins: [
-    `gatsby-transformer-remark`,
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = path.resolve(`src/templates/post.js`)
+
+  const result = await graphql(`
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/src/content`,
-      },
-    },
-  ]
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    console.log(result.errors)
+    throw new Error("Things broke, see console output above")
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {}, // additional data can be passed via context
+    })
+  })
 }
+
