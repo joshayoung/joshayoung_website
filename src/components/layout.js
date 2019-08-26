e315745ec1f3cee8e52925d5fdc56f1c7f02115e
Josh Young <j22cal@gmail.com>
2019-08-26 18:42:04 -0500

    Remove layout.css, add structure css, include in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "normalize.css"
+import "../styles/structure.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
