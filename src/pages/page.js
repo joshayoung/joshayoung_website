51dcfb698ea7b43d85d097e199ba96bbb36e40f9
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-14 09:54:52 -0600

    Started spiking out a new design

---
+++ src/pages/page.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  Link
+} from "gatsby"
+
+import Layout2 from "../components/layout2"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout2>
+    <SEO title="Page two" />
+    <p>My name is Josh. I am a Web Developer. Here I write about web development and my current tech projects. Check out my Blog to see what I am writing or my Projects to see what I am building.</p>
+  </Layout2>
+)
+
+export default ThirdPage
