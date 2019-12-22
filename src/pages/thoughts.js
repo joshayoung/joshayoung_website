bb951fda383650b540286aae5e81050899d2c4a0
Josh Young <j22cal@gmail.com>
2019-12-22 15:35:03 -0600

    Add classes page and warning css

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -8,7 +8,7 @@ export default () => {
   return (
     <Layout>
       <h1 className='header'>Thoughts</h1>
-      <article class='thought'>
+      <article>
         <p>Here I document concise posts about web development or programming that I am learning or have recently discovered. Many times these entries are very short, as I am not a prolific blogger. However, hopefully something I list here will be helpful to others.</p>
       </article>
       {posts.map(post => (
