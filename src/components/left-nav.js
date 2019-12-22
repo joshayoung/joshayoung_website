88f219b83aaaec287d8e040524a221bf4f344f0b
Josh Young <j22cal@gmail.com>
2019-12-22 16:26:13 -0600

    Add left hand navigation for wider screens

---
+++ src/components/left-nav.js
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import React from "react";
+import NavLinks from "../components/nav-links";
+import { css } from '@emotion/core';
+
+const blue = "#3385ff";
+
+const LeftNav = ({ siteTitle }) => (
+  <nav css={css`
+  width: 200px;
+  margin: 150px 10px 0 10px;
+  display: none;
+  @media (min-width: 700px) {
+    display: block;
+  }
+
+  li {
+    display: block;
+    span {
+      background: red;
+      border-bottom: 1px solid #ccc;
+    }
+    a {
+      display: block;
+      text-align: center;
+      padding: 10px;
+      &.active {
+        background: none;
+        color: ${blue};
+        font-style: italic;
+      }
+    }
+  }
+  `}>
+    <NavLinks />
+  </nav >
+)
+
+LeftNav.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+LeftNav.defaultProps = {
+  siteTitle: ``,
+}
+
+export default LeftNav;
---
+++ src/components/nav-links.js
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "gatsby";
+import styled from "@emotion/styled";
+
+const NavLink = styled(Link)`
+  font-size: 14px;
+  color: ${props => props.color || "#000;"};
+  &.active {
+    background: #fff;
+  }
+`;
+
+const NavLinks = ({ siteTitle }) => (
+  <ul>
+    <li>
+      <NavLink activeClassName='active' to="/">Home</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/thoughts">Thoughts</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/certifications">Certifications</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/projects">Projects</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/classes">Classes</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/resume">Resume</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/me">About</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/snippets">Snippets</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/definitions">Definitions</NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName='active' to="/resources">Resources</NavLink>
+    </li>
+  </ul>
+)
+
+NavLinks.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+NavLinks.defaultProps = {
+  siteTitle: ``,
+}
+
+export default NavLinks;
