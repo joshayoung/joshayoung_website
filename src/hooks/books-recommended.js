2eba9d743f4714b428eddec68aa3783b47220f42
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-22 22:33:27 -0600

    Add book list

---
+++ src/hooks/books-recommended.js
@@ -0,0 +1,33 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const BooksRecommended = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allBooksCategorizedJson {
+      nodes {
+        hide_from_recommended
+        id
+        title
+        data {
+          author
+          title
+          url
+          display
+          short_title
+          recommend
+          truncated_authors
+        }
+      }
+    }
+  }
+  `);
+
+  return data.allBooksCategorizedJson.nodes.map(data => ({
+    hide_list: data.hide_from_recommended,
+    id: data.id,
+    title: data.title,
+    data: data.data
+  }));
+};
+
+export default BooksRecommended;
