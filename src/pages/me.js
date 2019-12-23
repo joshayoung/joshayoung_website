0f300beb5c1e7fdc4a0df40def78f2fb8dbe2044
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-22 19:07:13 -0600

    Pull treehouse class list from data file, add yellow background

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
+import ListTreehouse from "../hooks/list-treehouse";
 
 export default ({ children }) => {
+  const treehouse = ListTreehouse();
   return (
     <Layout>
       <h1 className='header'>About</h1>
       {children}
       <img width="100" height="100" src='certs/resized/SecurityPlus_Logo_Certified_CE.png' />
+      {treehouse.map(treehouse => (
+        <h3>{treehouse.title}</h3>
+      ))}
     </Layout>
   )
 }
