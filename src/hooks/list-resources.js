c96d88bdfccb40fbae233a32f0d7f228e3343d13
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-22 20:01:30 -0600

    Add resources

---
+++ src/hooks/list-resources.js
@@ -0,0 +1,29 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const ListResources = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allResourcesJson {
+      nodes {
+        id
+        title
+        class
+        data {
+          title
+          display
+          url
+        }
+      }
+    }
+  }
+  `);
+
+  return data.allResourcesJson.nodes.map(data => ({
+    id: data.id,
+    title: data.title,
+    class: data.class,
+    data: data.data
+  }));
+};
+
+export default ListResources;
