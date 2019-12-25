d20c5daf1824469c448194912f91790baaa65358
Josh Young <j22cal@gmail.com>
2019-12-24 19:45:05 -0600

    WIP: highlight.js broken

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,16 +6,42 @@
 
 // You can delete this file if you're not using it
 
-const hljs = require("./node_modules/highlight.js");
+const hljs = require('./node_modules/highlight.js');
 
 exports.onClientEntry = () => {
-  window.addEventListener('load', () => {
-    document.body.className = document.body.className.replace(/\bno-js\b/, '');
-  });
+  window.onload = () => {
+    debugger;
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  }
+  // document.addEventListener('DOMContentLoaded', (event) => {
+  // debugger;
+  // document.body.className = document.body.className.replace(/\bno-js\b/, '');
+  //   document.querySelectorAll('pre code').forEach((block) => {
+  //     hljs.highlightBlock(block);
+  //   });
+  // });
+  // window.addEventListener('load', () => {
+  //   document.querySelectorAll('pre code').forEach((block) => {
+  //     hljs.highlightBlock(block);
+  //   });
+  // });
 
+  // document.addEventListener('DOMContentLoaded', (event) => {
+  // hljs.initHighlightingOnLoad();
+  // hljs.configure({
+  //   useBR: true
+  // });
+  // });
+};
+exports.onInitialClientRender = () => {
+  console.log("ReactDOM.render has executed")
   document.addEventListener('DOMContentLoaded', (event) => {
+    debugger;
+    // document.body.className = document.body.className.replace(/\bno-js\b/, '');
     document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
     });
   });
-}
+};
