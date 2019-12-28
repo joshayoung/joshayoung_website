fe2160033652d0d2edf4d969ce93c242f6a6d2a5
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-28 11:50:28 -0600

    Start moving CSS to the components and create a 'main' component

---
+++ src/components/main.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { css } from '@emotion/core';
+
+const Main = ({ children }) => (
+  <main
+    css={css`
+    margin: 1rem auto;
+    max-width: 90vw;
+    width: 700px;
+    @media (min-width: 600px) {
+        border-left: 1px solid #dedede;
+        padding-left: 30px;
+    }
+    @media (min-width: 700px) {
+      margin: 0;
+      margin-top: 50px;
+    }
+    > p:first-of-type {
+      line-height: 2;
+      font-size: 25px;
+      background: #eaeaea;
+      border-bottom: 1px solid #3385ff;
+      box-shadow: 1px 1px 1px #ccc;
+      margin-bottom: 50px;
+      padding: 0 10px;
+      font-family: 'Acme', sans-serif;
+      &:first-letter {
+        color: #3385ff;
+      }
+    }
+  `}>
+    {children}
+  </main>
+)
+
+export default Main;
