2b3abb061d4267fbde3a4d0983f38739936ad20c
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-28 13:49:55 -0600

    Style thoughts and add page intro text

--- src/items/thought.js
+++ src/items/thought.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { css } from '@emotion/core';
+
+const blue = "#3385ff";
 
 const Thought = ({ post }) => (
-  <>
+
+  <div css={css`
+    border-bottom: 3px solid #ccc;
+    margin-bottom: 40px;
+
+    a {
+      color: ${blue};
+    }
+  `}>
     <h3><Link to={post.path}>{post.title}</Link></h3>
     <p>{post.description}</p>
-    <div>{post.date}</div>
-  </>
+    <div
+      css={css`
+        font-size: 12px;
+      `}
+    >{post.date}</div>
+  </div>
 );
 
 export default Thought;
