3217e02e540729be49d478de01030a8dc6dfb532
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-12-27 19:20:49 -0600

    Add gatsby-remark-images and the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,9 @@ module.exports = {
         defaultLayouts: {
           default: require.resolve("./src/components/layout-markdown.js"),
         },
+        gatsbyRemarkPlugins: [
+          { resolve: 'gatsby-remark-images' }
+        ]
       },
     },
     `gatsby-plugin-emotion`,
@@ -52,6 +55,20 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
