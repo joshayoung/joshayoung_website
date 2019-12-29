facfca1f93a7ca6c87ac77d97e29b3c3fd29a0f9
Josh Young <j22cal@gmail.com>
2019-12-28 21:54:46 -0600

    chechin 2

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -2,19 +2,27 @@ import React, { useState, useEffect } from "react";
 import Layout from "../components/layout";
 
 const getIt = (url) => {
+  let token = window.localStorage.getItem('token');
+  return fetch(url,
+    { headers: { 'Authorization': 'token ' + token } }
+  ).then(response => response.json())
+    .then(result => {
+      return result[0].name;
+    })
+};
 
+const githubData = async () => {
+  const data = await getIt('https://api.github.com/users/joshayoung/repos');
+  return data;
 };
 
 const Stats = () => {
   const [repos, setRecentlyUpdatedRepos] = useState(0)
-  let token = window.localStorage.getItem('token');
   useEffect(() => {
-    fetch('https://api.github.com/users/joshayoung/repos',
-      { headers: { 'Authorization': 'token ' + token } }
-    ).then(response => response.json())
-      .then(result => {
-        setRecentlyUpdatedRepos(result[0].name);
-      })
+    const data = githubData();
+    data.then((res) => {
+      setRecentlyUpdatedRepos(res);
+    });
   }, []);
 
   return (
