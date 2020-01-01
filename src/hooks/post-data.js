4d49dfb2341e086ed4fae62f158401b0b1557c6b
Josh Young <j22cal@gmail.com>
2020-01-01 10:48:43 -0600

    Change the 'Lastest Posts' to the top 3 dynamically

---
+++ src/hooks/post-data.js
@@ -0,0 +1,27 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const PostData = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMdx(sort: {order: DESC, fields: frontmatter___date}, limit: 3) {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            description
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    date: post.frontmatter.date,
+    description: post.frontmatter.description,
+    path: post.frontmatter.path
+  }));
+};
+
+export default PostData;
---
+++ src/output/posts-output.js
@@ -0,0 +1,19 @@
+import React from "react";
+import List from "../components/list";
+import PostData from "../hooks/post-data";
+import { Link } from 'gatsby';
+
+export default () => {
+  const posts = PostData();
+
+  return (
+    <List>
+      <header>Latest Posts</header>
+      <ul>
+        {posts.map(post => (
+          <li><Link to={post.path}>{post.title}</Link></li>
+        ))}
+      </ul>
+    </List >
+  );
+};
