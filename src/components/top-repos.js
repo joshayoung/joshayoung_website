9bf48d70427ae8c235984169c1738c6efbe5484a
Josh Young <j22cal@gmail.com>
2020-01-07 19:17:11 -0600

    Display recent repos on stats page

---
+++ src/components/top-repos.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/layout";
+
+const top = function (data) {
+  let all = [];
+  data.forEach((repo, value) => {
+    all.push({
+      'name': repo.name,
+      'url': "<a href='" + repo.html_url + "'>" + repo.name + "</a>",
+      'raw_url': repo.url,
+      'updated': repo.updated_at,
+      'language': repo.language,
+      'created_at': repo.created_at.split("T")[0]
+    });
+  });
+  all.sort(function (a, b) {
+    return new Date(b.updated) - new Date(a.updated)
+  });
+  return all.slice(0, 3);
+};
+
+const getData = () => {
+  const [repos, setRecentlyUpdatedRepos] = useState("no data");
+  const [results, setResults] = useState(false);
+
+  async function getRepoList() {
+    let token = process.env.GATSBY_GITHUB_TOKEN;
+    const response = await fetch(
+      'https://api.github.com/users/joshayoung/repos'
+      , { headers: { 'Authorization': 'token ' + token } });
+    const json = await response.json();
+    let t = top(json)
+    setRecentlyUpdatedRepos(t);
+    setResults(true);
+  }
+
+  useEffect(() => {
+    getRepoList()
+  }, []);
+
+  return { repos, results };
+};
+
+export default () => {
+  const { repos, results } = getData();
+  return (
+    <>
+      {results ?
+        <ul>
+          {repos.map((val) => (
+            <li key={val.url}>{val.name}</li>
+          ))}
+        </ul>
+        : <ul><li>nothing</li></ul>
+      }
+    </>
+  );
+};
--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,32 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/layout";
+import TopRepos from "../components/top-repos";
+import List from "../components/list";
 
-const getIt = (url) => {
-  // let token = window.localStorage.getItem('token');
-  let token = process.env.GATSBY_GITHUB_TOKEN;
-
-  return fetch(url,
-    { headers: { 'Authorization': 'token ' + token } }
-  ).then(response => { return response.json() })
-};
-
-const Stats = () => {
-  const [repos, setRecentlyUpdatedRepos] = useState("no data")
-  useEffect(async () => {
-    const data = await getIt('https://api.github.com/users/joshayoung/repos');
-    setRecentlyUpdatedRepos(data[0].name);
-  }, []);
-
+export default () => {
   return (
     <Layout>
-      {/* TODO: Create a component here that you can use to loop through the result set */}
-      <p>This page holds some of the API data for projects I am currently or have worked on in the past.</p>
-      <div>Data: {repos}</div>
-      <div>
-        {`${process.env.GATSBY_API_URL}`}
-      </div>
+      <List>
+        <header>Recently Updated Repos</header>
+        <TopRepos />
+      </List>
     </Layout>
   );
-}
-
-export default Stats;
+};
