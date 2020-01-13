251d918b0c6304f64d1aa1d241e4ffb6594895c0
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-01-13 16:46:18 -0600

    Add basic tagging

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,14 @@
  */
 
 const path = require(`path`)
+const _ = require('lodash')
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = path.resolve(`src/templates/post.js`)
+  const tagTemplate = path.resolve("src/templates/tags.js")
+
 
   const result = await graphql(`
     {
@@ -21,8 +24,14 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           node {
             frontmatter {
               path
+              tags
             }
           }
+        }
+      }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
         }
       }
     }
@@ -39,4 +48,17 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       context: {}, // additional data can be passed via context
     })
   })
+
+    // Extract tag data from query
+    const tags = result.data.tagsGroup.group
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+        component: tagTemplate,
+        context: {
+          tag: tag.fieldValue,
+        },
+      })
+    })
 }
