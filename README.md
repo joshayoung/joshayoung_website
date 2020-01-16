0b2773842b701ea89b44a1576836d341b318cd51
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-01-16 12:20:08 -0600

    Start adding archived repo list

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # Website for joshayoung.com
 
 ### Start Site:
-* npm run develop
+
+- npm run develop
+- http://localhost:8080
 
 ### Left Off:
-* https://www.gatsbyjs.org/tutorial/part-two/ (using css modules)
+
+- https://www.gatsbyjs.org/tutorial/part-two/ (using css modules)
 
 ### Resources:
-* https://www.gatsbyjs.org/tutorial/part-five/
-https://www.gatsbyjs.org/tutorial/
+
+- https://www.gatsbyjs.org/tutorial/part-five/
+  https://www.gatsbyjs.org/tutorial/
 
 ### Notes
-* See 'note.txt' for details so far.
-* The stylesheet that is imported into each component is not isolated to that component. It is globally combined into one massive stylesheet for the whole site. If you want your styles to be isolated per component, you will need to add inline styles to your component (I am sure there is another way to do this, but this is what I know at this point).
+
+- See 'note.txt' for details so far.
+- The stylesheet that is imported into each component is not isolated to that component. It is globally combined into one massive stylesheet for the whole site. If you want your styles to be isolated per component, you will need to add inline styles to your component (I am sure there is another way to do this, but this is what I know at this point).
--- src/snippets/tags.js
+++ src/snippets/tags.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Box from "../components/box";
-import { Link } from "gatsby";
-import { graphql, useStaticQuery } from 'gatsby';
+import Box from "../components/box"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 function test() {
-const data = useStaticQuery(graphql`
-    query{
+  const data = useStaticQuery(graphql`
+    query {
       allMarkdownRemark {
         totalCount
         edges {
@@ -15,34 +15,39 @@ const data = useStaticQuery(graphql`
             }
           }
         }
-
       }
     }
-  `);
+  `)
 
-  let tags = [];
-  data.allMarkdownRemark.edges.map((dta) => {
-    let tg = dta.node.frontmatter.tags;
+  let tags = []
+  data.allMarkdownRemark.edges.map(dta => {
+    let tg = dta.node.frontmatter.tags
     tg.forEach(element => {
       if (tags.includes(element.toLowerCase())) {
-        return;
+        return
       }
-      tags.push(element.toLowerCase());
-    });
-  });
-  return tags;
+      tags.push(element.toLowerCase())
+    })
+  })
+  return tags
 }
 
 export default () => {
-  let dta = test();
+  let dta = test()
   return (
     <Box>
-      <header><Link to="/tags">Tags</Link></header>
+      <header>
+        <Link to="/tags">Tags</Link>
+      </header>
       <ul aria-labelledby="tags">
         {dta.map(tag_link => (
-          <li><a href={`/tags/${tag_link}`}>{tag_link.charAt(0).toUpperCase() + tag_link.slice(1)}</a></li>
+          <li key={tag_link}>
+            <a href={`/tags/${tag_link}`}>
+              {tag_link.charAt(0).toUpperCase() + tag_link.slice(1)}
+            </a>
+          </li>
         ))}
       </ul>
     </Box>
   )
-};
+}
