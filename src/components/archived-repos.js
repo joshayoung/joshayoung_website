a4966e80844971a040f4e9024ec7b84aad8f0fcb
Josh Young <j22cal@gmail.com>
2020-01-20 12:17:37 -0600

    Cleanup 'archived' repo list code

--- src/components/archived-repos.js
+++ src/components/archived-repos.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react"
 
+const getTheTags = async (url) => {
+    let token = process.env.GATSBY_GITHUB_TOKEN
+      const response2 = await fetch(url, {
+        headers: { Authorization: "token " + token },
+      })
+      return response2.json();
+}
 const getData = () => {
   const [repos, setRecentlyUpdatedRepos] = useState("no data")
   const [results, setResults] = useState(false)
@@ -13,18 +20,18 @@ const getData = () => {
     const json = await response.json()
     let tags = []
     for (var i = 0; i < json.length; i++) {
-      tags.push(json[i].tags_url)
+      tags.push({name: json[i].name, url: json[i].tags_url})
     }
     let tall = []
     let json2
-    for (var i = 0; i < tags.length; i++) {
-      const response2 = await fetch(tags[i], {
-        headers: { Authorization: "token " + token },
-      })
-      json2 = await response2.json()
+    for (var k = 0; k < tags.length; k++) {
+      json2 = await getTheTags(tags[k].url);
 
-      if (json2.length > 0) {
-        tall.push(json2[0])
+      for(var j = 0; j < json2.length; j++) {
+        if (json2[j].name === "archived") {
+          tall.push(tags[k].name)
+          break;
+        }
       }
     }
     setRecentlyUpdatedRepos(tall)
@@ -46,8 +53,8 @@ export default () => {
       {results ? (
         <ul>
           {repos.map(val => (
-            <li key={val.commit.sha}>
-              <div dangerouslySetInnerHTML={{ __html: val.name }} />
+            <li key={val}>
+              <div dangerouslySetInnerHTML={{ __html: val }} />
             </li>
           ))}
         </ul>
