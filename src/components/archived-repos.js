ad8cc2a3f52a56a0e8b971b9b869494f08f88aae
Josh Young <j22cal@gmail.com>
2020-01-22 17:27:54 -0600

    More archive repo list cleanup

--- src/components/archived-repos.js
+++ src/components/archived-repos.js
@@ -1,52 +1,65 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-const getTheTags = async (url) => {
-    let token = process.env.GATSBY_GITHUB_TOKEN
-      const response2 = await fetch(url, {
-        headers: { Authorization: "token " + token },
-      })
-      return response2.json();
-}
+const getRepos = async () => {
+  let token = process.env.GATSBY_GITHUB_TOKEN;
+  const response = await fetch(
+    "https://api.github.com/users/joshayoung/repos",
+    { headers: { Authorization: "token " + token } }
+  );
+  return response.json();
+};
+
+const getTheTags = async url => {
+  let token = process.env.GATSBY_GITHUB_TOKEN;
+  const response = await fetch(url, {
+    headers: { Authorization: "token " + token },
+  });
+  return response.json();
+};
+
+const archivedTags = (name, data) => {
+  let tags = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name === "archived") {
+      tags.push(name);
+      continue;
+    }
+  }
+  return tags;
+};
+
 const getData = () => {
-  const [repos, setRecentlyUpdatedRepos] = useState("no data")
-  const [results, setResults] = useState(false)
+  const [repos, setRecentlyUpdatedRepos] = useState("no data");
+  const [results, setResults] = useState(false);
 
   async function getRepoList() {
-    let token = process.env.GATSBY_GITHUB_TOKEN
-    const response = await fetch(
-      "https://api.github.com/users/joshayoung/repos",
-      { headers: { Authorization: "token " + token } }
-    )
-    const json = await response.json()
-    let tags = []
-    for (var i = 0; i < json.length; i++) {
-      tags.push({name: json[i].name, url: json[i].tags_url})
-    }
-    let tall = []
-    let json2
-    for (var k = 0; k < tags.length; k++) {
-      json2 = await getTheTags(tags[k].url);
-
-      for(var j = 0; j < json2.length; j++) {
-        if (json2[j].name === "archived") {
-          tall.push(tags[k].name)
-          break;
-        }
+    const data = await getRepos();
+    let tags = [];
+    data.forEach(val => {
+      tags.push({ name: val.name, url: val.tags_url });
+    });
+    let archived = [];
+    let data_tags;
+    for (let i = 0; i < tags.length; i++) {
+      data_tags = await getTheTags(tags[i].url);
+      let archived_repo = archivedTags(tags[i].name, data_tags);
+      if (archived_repo.length > 0) {
+        archived.push(archived_repo);
       }
     }
-    setRecentlyUpdatedRepos(tall)
-    setResults(true)
+    setRecentlyUpdatedRepos(archived);
+    setResults(true);
   }
 
   useEffect(() => {
-    getRepoList()
-  }, [])
+    getRepoList();
+  }, []);
 
-  return { repos, results }
-}
+  return { repos, results };
+};
 
 export default () => {
-  const { repos, results } = getData()
+  const { repos, results } = getData();
   return (
     <>
       {/* <pre>{JSON.stringify(repos)}</pre> */}
@@ -60,9 +73,9 @@ export default () => {
         </ul>
       ) : (
         <ul>
-          <li>nothing</li>
+          <li>loading...</li>
         </ul>
       )}
     </>
-  )
-}
+  );
+};
