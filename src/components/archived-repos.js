55b0be96ad7d8b6e891603dd9bd0a46036b3b60d
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-01-24 11:56:47 -0600

    Moved methods for archived repos to utilty file

--- src/components/archived-repos.js
+++ src/components/archived-repos.js
@@ -1,50 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { getData } from "../utilities/api_requests"
-
-const repoName = name => {
-  return name.zipball_url.split("/")[5];
-};
-
-const archivedTags = data => {
-  let archived = [];
-  data.forEach(tg => {
-    let repo = tg.filter(value => value.name === "archived");
-    if (repo.length > 0) {
-      archived.push(repoName(repo[0]));
-    }
-  });
-  return archived;
-};
-
-const tags = data => {
-  let allTags = [];
-  data.forEach(tag => {
-    allTags.push({ name: tag.name, url: tag.tags_url });
-  });
-  return allTags;
-};
+import { tags, archivedRepos, REPO_URL, getData, archivedTags } from "../utilities/api_requests"
 
 const getTheData = () => {
   const [repos, setRecentlyUpdatedRepos] = useState("no data");
   const [results, setResults] = useState(false);
 
   async function getRepoList() {
-    const data = await getData('https://api.github.com/users/joshayoung/repos');
-    let allTags = tags(data);
-    let tagsUrl = [];
-    allTags.forEach(tag => {
-      tagsUrl.push(getData(tag.url));
-    });
-
-    let archived_repos = Promise.all(tagsUrl).then(all => {
-      let archived = [];
-      all.forEach(t => {
-        archived.push(t);
-      });
-      return archived.filter(a => a.length !== 0);
-    });
-
-    archived_repos.then(ar => {
+    const data = await getData(REPO_URL);
+    let tagsUrl = tags(data);
+    archivedRepos(tagsUrl).then(ar => {
       const results = archivedTags(ar);
       setRecentlyUpdatedRepos(results);
       setResults(true);
