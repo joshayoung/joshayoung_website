96b18957c78bacf4abb2a4c767f2c8cb1d3f78bf
Josh Young <j22cal@gmail.com>
2020-01-29 20:14:56 -0600

    WIP: start adding feature complete repos

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import TopRepos from "../components/top-repos";
 import ArchivedRepos from "../components/archived-repos";
+import FeatureComplete from "../components/feature-complete-repos";
 import List from "../components/list";
 
 export default () => {
@@ -13,6 +14,9 @@ export default () => {
 
         <header>Archived Repos</header>
         <ArchivedRepos />
+
+        <header>Feature Complete Repos</header>
+        <FeatureComplete />
       </List>
     </Layout>
   );
