e29aa2a216589d8cd354a9cbea62098bc5d4becc
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-02-13 11:07:17 -0600

    Remove old navigation styles

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,94 +6,9 @@ const blue = "#3385ff";
 
 const Nav = () => (
   <nav css={css`
-    margin-left: 100%;
-    position: relative;
-    /* TODO: fix this: */
-    left: -51px;
 
-    label, #menu {
-      display: none;
-    }
-
-    label {
-      z-index: 200;
-      position: relative;
-    }
-
-    ul {
-      padding-top: 68px;
-      position: fixed;
-      background: ${blue};
-      background: #ececec;
-      height: 100%;
-      z-index: 100;
-      width: 250px;
-      margin-top: -68px;
-      /* TODO: Correct this so the animation works on page load: */
-      // animation: close 0.5s ease;
-      right: -250px;
-      animation-fill-mode: forwards;
-      border-left: solid 3px ${blue};
-      @media (min-width: 700px) {
-        display: none;
-      }
-    }
-
-    ul li {
-      display: block;
-      a.active {
-        background: #fff;
-      }
-    }
-
-    li:nth-of-type(2n) a {
-      background: #e6e6e6;
-      &.active {
-        background: #fff;
-      }
-      &:hover {
-        background: #fff;
-      }
-    }
-
-    a {
-      display: block;
-      padding: 10px;
-      font-size: 18px;
-      &:hover {
-        background: #fff;
-      }
-    }
-
-    label {
-      display: inline-block;
-      font-size: 34px;
-      &:hover {
-        cursor: pointer;
-      }
-      @media (min-width: 700px) {
-        display: none;
-      }
-    }
-
-    input:checked ~ ul {
-      animation: open 0.5s ease;
-      animation-fill-mode: forwards;
-    }
-
-    @keyframes open {
-      from {right: -250px;}
-      to {right: 0;}
-     }
-
-     @keyframes close {
-      from {right: 0;}
-      to {right: -250px;}
-     }
   `}
   >
-    <label className='menu' htmlFor='menu'>&#9776;</label>
-    <input type='checkbox' id='menu' />
     <NavLinks />
   </nav>
 )
