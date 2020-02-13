c6de14c90fa304068c3ebfe2554af9e7b06b2fea
Josh Young <j22cal@gmail.com>
2020-02-13 09:50:37 -0600

    Remove search component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
-import Search from "../components/SearchContainer"
 
 export default () => {
   return (
     <Layout>
       <h1>Search</h1>
       <p>To search this, site type your query in the search box below. If you are not able to find your query, please try the site navigation.</p>
-        <Search />
     </Layout>
   );
 };
