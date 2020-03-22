9bd1901b8a894c0ce6ee2897bc22c571a6e3a6d0
Josh Young <j22cal@gmail.com>
2020-03-21 19:25:44 -0500

    Remove left nav styles and extra navigation

--- src/components/left-nav.js
+++ src/components/left-nav.js
@@ -6,30 +6,7 @@ const blue = "#3385ff";
 
 const LeftNav = () => (
   <nav css={css`
-  width: 130px;
-  margin: 170px 10px 0 52px;
-  display: none;
-  @media (min-width: 700px) {
-    display: block;
-  }
 
-  li {
-    display: block;
-    span {
-      background: red;
-      border-bottom: 1px solid #ccc;
-    }
-    a {
-      display: block;
-      text-align: right;
-      padding: 10px;
-      &.active {
-        background: none;
-        color: ${blue};
-        font-style: italic;
-      }
-    }
-  }
   `}
     className="left-nav">
     <NavLinks />
