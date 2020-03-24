3c4c1aecccc47af29ebe3b6a7f7f40768394be8b
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-03-23 21:15:55 -0500

    Clean up book list - further cleanup later

--- src/items/book-recommended.js
+++ src/items/book-recommended.js
@@ -1,41 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BookRecommended = ({ title, hide, post }) => {
-  const fun = function (data) {
+class BookRecommended extends Component {
+  constructor() {
+    super();
+  }
+
+  author(data) {
     if (data.truncated_authors) {
       return " by" + data.truncated_authors;
     } else if (data.author !== '') {
       return " by " + data.author;
     }
   }
-  return (
-    <>
-      {hide !== true ?
+
+  bookTitle(book) {
+    if(book.short_title) {
+      return (
         <>
-          <h5>{title}</h5>
+          {book.short_title}
+          {this.author(book)}
+        </>
+      )
+    } else {
+      return (
+        <>
+          {book.title}
+          {this.author(book)}
+        </>
+      )
+    }
+  }
+
+  theBook(book, i) {
+    if (book.display && book.recommend === true) {
+      return (
+        <li key={i}>
+          {this.bookTitle(book)}
+        </li>
+      );
+    }
+  }
+
+  message() {
+    if (this.props.hide !== true) {
+      return (
+        <>
+          <h5>{this.props.title}</h5>
           <ul>
-            {post.map((book, i) => (
-              <>
-                {
-                  book.display && (book.recommend === true) ?
-                    <li key={i}>
-                      {book.short_title ?
-                        <>{book.short_title}</>
-                        :
-                        <>{book.title}</>
-                      }
-                      {fun(book)}
-                    </li>
-                    :
-                    ''
-                }
-              </>
+            {this.props.post.map((book, i) => (
+              this.theBook(book, i)
             ))}
           </ul>
         </>
-        : ''}
-    </>
-  )
+      )
+    }
+  }
+
+  render() {
+    return (
+      <>
+        {this.message()}
+      </>
+    )
+  }
 };
 
 export default BookRecommended;
