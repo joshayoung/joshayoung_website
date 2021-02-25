d7e954d1c1a41d67cfdcab73695d9b56b331a8f7
Josh Young <j22cal@gmail.com>
2021-02-25 13:31:02 -0600

    Remove highlight.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,16 +6,5 @@
 
 // You can delete this file if you're not using it
 
-const hljs = require('./node_modules/highlight.js');
 require("prismjs/themes/prism-solarizedlight.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
-
-exports.onClientEntry = () => {
-  window.onload = () => {
-    document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightBlock(block);
-    });
-  }
-};
-
-exports.onInitialClientRender = () => { };
