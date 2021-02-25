c2bdc111753d160820dad3cae3bea78783938434
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-02-25 13:53:03 -0600

    Cleanup config file

--- src/components/inner.js
+++ src/components/inner.js
@@ -15,7 +15,7 @@ const Inner = ({ children }) => (
       }
     `}
   >
-    <a id="top" href="#1"></a>
+    <a id="top" href="#1" aria-label="top"></a>
     <Header />
     <button className="hamburger" css={css`
       cursor: pointer;
