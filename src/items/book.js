621f8bc67a269491b20ac344a53d83888bd00a4b
Josh Young <j22cal@gmail.com>
2021-03-27 10:32:47 -0500

    Refactor books output to eliminate empty list items
    Closes #81

--- src/items/book.js
+++ src/items/book.js
@@ -1,28 +1,41 @@
 import React from "react";
 
 const Book = ({ title, post }) => {
-  const fun = function(data) {
-    if (data.truncated_authors) {
-      return " by " + data.truncated_authors;
-    } else if (data.author !== "") {
-      return " by " + data.author;
+  const fun = function(book) {
+    if (book.truncated_authors) {
+      return " by " + book.truncated_authors;
+    } else if (book.author !== "") {
+      return " by " + book.author;
     }
   };
+
+  const bookTitle = function(book) {
+    let title = '';
+    if (book.short_title) {
+      title = book.short_title
+    } else {
+      title = book.title
+    }
+    return title;
+  }
+
+  const theBook = function(book) {
+    if (!book.display) {
+      return null;
+    }
+    return (
+      <li> {bookTitle(book)} {fun(book)} </li>
+    )
+  }
+
   return (
     <>
       <h5>{title}</h5>
       <ul>
         {post.map((book, i) => (
-          <li key={i}>
-            {book.display ? (
-              <>
-                {book.short_title ? <>{book.short_title}</> : <>{book.title}</>}
-                {fun(book)}
-              </>
-            ) : (
-              ""
-            )}
-          </li>
+          <div key={i}>
+            {theBook(book)}
+          </div>
         ))}
       </ul>
     </>
