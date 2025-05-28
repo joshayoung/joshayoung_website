85f7cc5a39a6aac90fdee0524a5e02edd499e69b
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-05-28 17:05:55 -0500

    Add a config file

--- rebuiltsite/README.md
+++ rebuiltsite/README.md
@@ -1,2 +1,6 @@
+## Steps
+* `npm install sass`
+
 ## Start the Local Server
 * `npx @11ty/eleventy --serve`
+npx @11ty/eleventy --formats=scss
---
+++ rebuiltsite/eleventy.config.js
@@ -0,0 +1,34 @@
+const path = require("node:path");
+const sass = require("sass");
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addTemplateFormats("scss")
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+
+		// opt-out of Eleventy Layouts
+		useLayouts: false,
+
+		compile: async function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			// Don’t compile file names that start with an underscore
+			if(parsed.name.startsWith("_")) {
+				return;
+			}
+
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes,
+				]
+			});
+
+			// Map dependencies for incremental builds
+			this.addDependencies(inputPath, result.loadedUrls);
+
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
+};
